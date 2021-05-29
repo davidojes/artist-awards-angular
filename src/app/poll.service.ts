6ffdeb3d42790956e6030cd5d7cc6b880c0dca9c
davidojes <ojesekho@sheridancollege.ca>
2021-05-28 23:17:30 -0400

    Add vote function in poll service

--- src/app/poll.service.ts
+++ src/app/poll.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root'
@@ -7,10 +8,20 @@ import { HttpClient } from '@angular/common/http';
 export class PollService {
   poll;
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private userService: UserService) { }
 
   async getPoll(id: string) {
     this.poll = await this.httpClient.get<any>('https://localhost:44399/api/poll/' + id).toPromise();
     return this.poll;
+  }
+
+  async vote(pollOptionId) {
+    var body = JSON.stringify({ "userId": this.userService.getUserId(), "pollOptionId": pollOptionId });
+    var response = await this.httpClient.post<any>('https://localhost:44399/api/poll/vote', body, {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json"
+      })
+    }).toPromise();
+    return response;
   }
 }
