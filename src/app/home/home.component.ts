2487826a9a1cde3fed37824558267c1fcf0d88f1
davidojes <ojesekho@sheridancollege.ca>
2021-05-31 00:26:00 -0400

    Redirect user to login page if they aren't authenticated

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private userService: UserService) { }
 
   ngOnInit(): void {
+    if(this.userService.getUserLoggedIn() == false) {
+      this.router.navigate(['login']);
+    }
   }
 
 }
